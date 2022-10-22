4481185340845a5e7ad5f6f243ff20d80a9a05f5
Yaroslav <yarikmeisan@yandex.com>
2022-10-22 16:43:13 +0300

    Changing QandA class

--- patriot.by-quiz/Assets/Scripts/QandA.cs
+++ patriot.by-quiz/Assets/Scripts/QandA.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "QuestionsAndAnswers", menuName = "Q&A Object")]
-[Serializable]
-public class QandA : ScriptableObject
+//[CreateAssetMenu(fileName = "QuestionsAndAnswers", menuName = "Q&A Object")]
+[System.Serializable]
+public class QandA
 {
-    public string question;
-    public string[] answers;
-    public int correctAnswer;
+        public string question;
+        public string[] answers;
+        public int correctAnswer;
 }
