7d0d3542bf5c5e8b8c71c83c8e3d8f9b5431b99d
Yaroslav <yarikmeisan@yandex.com>
2022-10-24 10:27:33 +0300

    Setting up animations

--- patriot.by-quiz/Assets/Scripts/SceneMngr.cs
+++ patriot.by-quiz/Assets/Scripts/SceneMngr.cs
@@ -5,8 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class SceneMngr : MonoBehaviour
 {
+    public Animator fadeIn;
+    public Animator fadeOut;
+    public GameObject fader;
     public void LoadGame()
     {
+        fader.SetActive(true);
+        fadeIn.SetTrigger("FadeIn");
         SceneManager.LoadScene(1);
         DontDestroyOnLoad(gameObject);
     }
